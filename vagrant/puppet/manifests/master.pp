a5917e1eb2ef5be69fcc8b2fb9d7ce68ee80138a
Alexander <iam.asm89@gmail.com>
2012-10-20 12:29:48 +0200

    Install rabbitmq via puppet

--- vagrant/puppet/manifests/master.pp
+++ vagrant/puppet/manifests/master.pp
@@ -1,5 +1,23 @@
 include php
+include stdlib
+
+class { 'apt':
+  always_apt_update => false
+}
 
 # PHP Extensions
 php::module { ['xdebug', 'curl'] :
 }
+
+# add apt repo for rabbitmq
+class { 'rabbitmq::repo::apt':
+  pin    => 900
+}
+
+# install rabbitmq
+class { 'rabbitmq::server':
+  port              => '5673',
+  delete_guest_user => false
+}
+
+Class['rabbitmq::repo::apt'] -> Class['rabbitmq::server']

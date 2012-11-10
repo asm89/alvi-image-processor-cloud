6222314724e858087d3a166b27301048cf0a0065
Alexander <iam.asm89@gmail.com>
2012-11-10 14:53:55 +0100

    Fix workloads (remove trailing ")") / remove old workload generator

--- src/Alvi/Bundle/ImageProcessor/JobSubmissionBundle/Command/ScheduleJobsCommand.php
+++ src/Alvi/Bundle/ImageProcessor/JobSubmissionBundle/Command/ScheduleJobsCommand.php
@@ -23,16 +23,7 @@ class ScheduleJobsCommand extends ContainerAwareCommand
             ->setName('alvi:image-processor:jobSubmit')
             ->setDescription('Schedule incoming image process jobs.')
             ->setDefinition(array(
-                new InputOption('workloadSize', '', InputOption::VALUE_OPTIONAL, "Amount of jobs to be submitted.", 3000),
-                new InputOption('normalJobSize', '', InputOption::VALUE_OPTIONAL, "Normal job size in microseconds.", 1000000),
-                new InputOption('burstJobSize', '', InputOption::VALUE_OPTIONAL, "Burst job size in microseconds.", 1000000),
-                new InputOption('burstInterval', '', InputOption::VALUE_OPTIONAL, "Burst interval number of normal message in between bursts.", 600),
-                new InputOption('burstCount', '', InputOption::VALUE_OPTIONAL, "Number of bursts.", 0),
-                new InputOption('burstSize', '', InputOption::VALUE_OPTIONAL, "Number of jobs in a burst.", 600),
-                new InputOption('jobInterupt', '', InputOption::VALUE_OPTIONAL, "Normal job interrupt in microseconds.", 300000),
-                new InputOption('recordWorkload', '', InputOption::VALUE_OPTIONAL, "Record the workload pattern for later use.", false),
-                new InputOption('openWorkload', '', InputOption::VALUE_OPTIONAL, "Read the workload pattern from a file.", false),
-                new InputOption('workloadFilepath', '', InputOption::VALUE_OPTIONAL, "Record filepath", '/data/workload.log'),
+                new InputArgument('workloadFilepath', InputArgument::REQUIRED, "Path to workload file to play"),
             ))
                 ->setHelp(<<<EOT
 The <info>%command.name%</info> command will schedule incoming image processing jobs.
@@ -49,137 +40,42 @@ EOT
     {
         $container = $this->getContainer();
 
-        //workload size (number of jobs submitted, bursts count as 1)
-        $workloadSize = $input->getOption('workloadSize');
-        $workloadSizeCounter = 0;
+        $path = $input->getArgument('workloadFilepath');
 
-        //if write workload to file create file or erase file
-        $workloadFilepath = $input->getOption('workloadFilepath');
-        $this->eraseCreateFile($input->getOption('recordWorkload'), $workloadFilepath);
-        
-        //normal message
-        //job size in microseconds
-        $normalJobSize = $input->getOption('normalJobSize');
-        //job size in microseconds
+        if (!is_file($path)) {
+            throw new \RuntimeException(sprintf("Specified workload '%s' is not a file.", $path));
+        }
 
-        //burst job
-        //job size in microseconds
-        $burstJobSize = $input->getOption('burstJobSize');
-        
-        $jobCounter = 0;
-        //burst interval
-        $burstInterval = $input->getOption('burstInterval');
-        //number of bursts
-        $burstCount = $input->getOption('burstCount');
-        //burst size
-        $burstSize = $input->getOption('burstSize');
-        //normal job timing in microseconds
-        $jobInterupt = $input->getOption('jobInterupt');
-
-        //connection with graphite
         $collector = $container->get('beberlei_metrics.collector.statsd');
-        
-        //rabbitMQ
         $rabbitMQ = $container->get('old_sound_rabbit_mq.upload_picture_producer');
-        
-        
-        //if read workoad from file is true
-        //replay a recorded workload
-        if($input->getOption('openWorkload') == true) {
-            $workload = $this->readWorkloadFromFile($workloadFilepath);
-            foreach($workload as $job) {
-                if($job['jobInterupt'] != 0) {
-                    usleep($job['jobInterupt']);
-                }
-                $job['submitTime'] = microtime(true);
-                $rabbitMQ->publish(serialize($job));
-                $collector->increment('alvi.jobs');
-                //send stats to graphite
-                $collector->flush();
+        $workload = $this->readWorkloadFromFile($path);
+        foreach($workload as $job) {
+            if ($job['jobInterupt'] != 0) {
+                usleep($job['jobInterupt']);
             }
-        }
-        //else generate workload
-        else {
-            //count the number of bursts
-            $burstCounter = 0;
-            while($workloadSizeCounter < $workloadSize)
-            {
-                $jobNormal = array('user_id' => 'normal', 'image_path' => '/path/to/new/pic.png', 'size' => (stats_dens_normal(rand(-5,5),0,1)*$normalJobSize), 'submitTime' => null, 'jobInterupt' => $jobInterupt);
-                //job interupt
-                usleep($jobInterupt);
-                if($jobCounter == $burstInterval)
-                {
-                    if($burstCounter < $burstCount) {
-                        //add message burst
-                        for($i=0;$i<$burstSize;$i++)
-                        {
-                            $jobBurst = array('user_id' => 'burst', 'image_path' => '/path/to/new/pic.png', 'size' => (stats_dens_normal(rand(-5,5),0,1)*$burstJobSize), 'submitTime' => null, 'jobInterupt' => 0);
-                            if($input->getOption('recordWorkload') == true) {
-                                $this->recordJob($jobBurst, $workloadFilepath);
-                            }
-                            $jobBurst['submitTime'] = microtime(true);
-                            $rabbitMQ->publish(serialize($jobBurst));
-                            $collector->increment('alvi.jobs');
-                            //send stats to graphite
-                            $collector->flush();
-                        }
-                        $burstCounter++;
-                    }
-                    //reset job counter
-                    $jobCounter = 0;
-                }
-                else
-                {
-                    if($input->getOption('recordWorkload') == true) {
-                        $this->recordJob($jobNormal, $workloadFilepath);
-                    }
-                    //add submit time to job
-                    $jobNormal['submitTime'] = microtime(true);
-                    //send normal message
-                    $rabbitMQ->publish(serialize($jobNormal));
-                    $collector->increment('alvi.jobs');
-                    //send stats to graphite
-                    $collector->flush();
 
-                    //increment jobcounter
-                    $jobCounter++;
-                }
-                //increment workloadsize counter
-                $workloadSizeCounter++;
-            }
+            $job['submitTime'] = microtime(true);
+
+            $rabbitMQ->publish(serialize($job));
+
+            $collector->increment('alvi.jobs');
+            $collector->flush();
         }
     }
-    
+
     /**
-     * @param job array $job
+     * @param $path
+     *
+     * @return array with jobs
      */
-    private function recordJob($job, $workloadFilepath) {
-        $file = fopen($workloadFilepath, 'a');
-        fwrite($file,json_encode($job)."\n");
-        fclose($file);
-    }
-    
-    /**
-     * return array with jobs
-     */
-    private function readWorkloadFromFile($workloadFilepath) {
-        $file = fopen($workloadFilepath, 'r');
+    private function readWorkloadFromFile($path) {
+        $file = fopen($path, 'r');
+
         $workload = array();
         while($line = fgets($file)) {
             $workload[] = json_decode($line, true);
         }
+
         return $workload;
-    }
-    /**
-     * erase and/or create the record file if 'recordWorkload' is true
-     */
-    private function eraseCreateFile($recordWorkload, $workloadFilepath) {
-        if($recordWorkload == true) {
-            if(file_exists($workloadFilepath)) {
-                unlink($workloadFilepath);
-            }
-            $file = fopen($workloadFilepath, 'w');
-            fclose($file);
-        }
     }
 }
